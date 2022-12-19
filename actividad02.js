e21367fc89a36b560076dd78c4cc6255205b0b85
christian-huelmo <christian.huelmo@hikko.is>
2022-12-19 11:34:22 -0300

    fix actividad02.js

--- actividad02.js
+++ actividad02.js
@@ -1,20 +1,35 @@
 const fs = require('fs')
 
 const leerArchivo = async nombre => {
-    let contenidoStr = await fs.promises.readFile(nombre, 'utf-8')
-    let contenidoObj = JSON.parse(contenidoStr)
-    let size = (await fs.promises.stat(nombre)).size
-    const info = {
-        'contenidoStr': contenidoStr,
-        'contenidoObj': contenidoObj,
-        'size': size
+    try {
+        let contenidoStr = await fs.promises.readFile(nombre, 'utf-8')
+        let contenidoObj = JSON.parse(contenidoStr)
+        let size = (await fs.promises.stat(nombre)).size
+        const info = {
+            'contenidoStr': contenidoStr,
+            'contenidoObj': contenidoObj,
+            'size': size
+        }
+        return info
+    } catch (e) {
+        throw new Error(e.message)
     }
-    return info
 }
 
 const resultado = async () => {
-    const info = await leerArchivo('package.json')
-    console.log(info)
+    try {
+        const info = await leerArchivo('package.json')
+        console.log(info)
+    } catch (e) {
+        throw new Error(e)
+    }
+    
 }
 
-resultado()
+(async function main() {
+    try {
+        await resultado()
+    } catch (e) {
+        console.error(e)
+    }
+})()
