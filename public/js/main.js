140ffd17127add01e480256b2086b7de175f669c
Christian <chuelmo@gmail.com>
2023-01-22 23:19:58 -0300

    frontend del desafio terminado

--- public/js/main.js
+++ public/js/main.js
@@ -51,12 +51,87 @@ function enviarDatos() {
     });
 }
 
+function deleteProduct(id) {
+  fetch(`/api/products/${id}`, {
+    method: "DELETE",
+  }).then((response) => {
+    response.json().then((res) => {
+      if (res.success) {
+        console.log(`Se eliminó el producto con id ${id}`);
+        socket.emit("new-product");
+      }
+    });
+  });
+}
+
+function updateProduct(e) {
+  console.log(
+    `Hiciste click en el boton update product con el id ${e.target.id}`
+  );
+  let campos = e.target.id.split(":");
+  let id = campos[1];
+  let nombre = campos[2];
+  let precio = campos[3];
+  let newName = document.getElementById("title_update").value;
+  let newPrice = document.getElementById("price_update").value;
+  nombre = newName || nombre;
+  precio = newPrice || precio;
+  fetch(`/api/products/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ product: { id: id, title: nombre, price: precio } }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    response.json().then((res) => {
+      if (res.success) {
+        console.log(`Se actualizó el producto con id ${id}`);
+        socket.emit("new-product");
+      }
+    });
+  });
+}
+
+function createFormUpdateProduct(id, nombre, precio) {
+  let formulario = `<form class="row g-3">
+                      <div class="col-12">
+                          <label for="id_update" class="form-label">Id</label>
+                          <input type="text" class="form-control" id="id_update" value="${id}" disabled>
+                      </div>
+                      <div class="col-12">
+                          <label for="title_update" class="form-label">Nombre</label>
+                          <input type="text" class="form-control" id="title_update" value="${nombre}">
+                      </div>
+                      <div class="col-12">
+                          <label for="price_update" class="form-label">Precio</label>
+                          <input type="number" class="form-control" id="price_update" value="${precio}">
+                      </div>
+                    </form>`;
+  let botones = `<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+  <button type="button" class="btn btn-primary" data-bs-dismiss="modal" id="update:${id}:${nombre}:${precio}">Guardar</button>`;
+  const modal_body = document.getElementById("modal_id");
+  modal_body.innerHTML = formulario;
+  const modal_footer = document.getElementById("id_modal_footer");
+  modal_footer.innerHTML = botones;
+  let button_update = document.getElementById(
+    `update:${id}:${nombre}:${precio}`
+  );
+  button_update.addEventListener("click", updateProduct);
+}
+
 function clickEnBorrar(e) {
   console.log(`hizo click en borrar con el id: ${e.target.id}`);
+  let id = e.target.id.slice(2);
+  deleteProduct(id);
 }
 
 function clickEnEdit(e) {
   console.log(`hizo click en edit con el id: ${e.target.id}`);
+  let campos = e.target.id.split(":");
+  let id = campos[0].slice(2);
+  let nombre = campos[1];
+  let precio = campos[2];
+  createFormUpdateProduct(id, nombre, precio);
 }
 
 function addActionsToButtons() {
@@ -97,7 +172,7 @@ socket.on("all_productos", (productos) => {
                 <td class="text-white">
                     <div class="actions cambiar" id="xa${p.id}">
                         <img src="./images/edit-front.png" alt="edit" id="xn${p.id}">
-                        <img src="./images/edit-back.png" alt="edit" class="img-top" id="xc${p.id}">
+                        <img src="./images/edit-back.png" alt="edit" class="img-top" id="xc${p.id}:${p.title}:${p.price}" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                     </div>
                     <div class="actions borrar" id="xt${p.id}">
                         <img src="./images/delete-front.png" alt="delete" id="xb${p.id}">
